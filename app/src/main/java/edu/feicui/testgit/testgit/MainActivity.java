3bf41042c27bf88078ea068fb491bfba80dfdc85
Administrator <1341936653@qq.com>
2016-10-19 14:34:04 +0800

    修改Main

--- app/src/main/java/edu/feicui/testgit/testgit/MainActivity.java
+++ app/src/main/java/edu/feicui/testgit/testgit/MainActivity.java
@@ -3,8 +3,14 @@ package edu.feicui.testgit.testgit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
